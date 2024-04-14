b63b85825f7c77dd723b36e2aad770cf63337050
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2024-04-14 12:19:48 -0400

    concurrent data fetching in simulatorFeed

--- src/components/SimulatorFeed.tsx
+++ src/components/SimulatorFeed.tsx
@@ -1,50 +1,3 @@
-// import { getLeagueStandingBySeason, getAllMatchesByLeagueSeason, getCurrentRound } from "@/lib/actions"
-// import Simulator from "./Simulator"
-// import LeagueCTA from "./LeagueCTA"
-
-// export default async function SimulatorFeed({ leagueId, currentSeason }: { leagueId: string, currentSeason: string }) {
-
-//     const rankingTable = await getLeagueStandingBySeason(leagueId, `${currentSeason}`)
-//     const allMatches = await getAllMatchesByLeagueSeason(leagueId, `${currentSeason}`)
-//     const currentRound = await getCurrentRound(leagueId, `${currentSeason}`)
-
-
-//     if (!currentRound || !Array.isArray(currentRound.response) || currentRound.response.length < 1) {
-//         return <div>Loading...</div>
-//     }
-
-//     if (!rankingTable || !rankingTable.response || !allMatches || !allMatches.response) {
-//         return <div>Loading...</div>
-//     }
-
-
-//     if (!rankingTable.response || !Array.isArray(rankingTable.response[0]?.league.standings[0])) {
-//         return (<LeagueCTA />)
-//     }
-
-//     const rankingTableData = rankingTable.response[0].league.standings[0]
-
-//     if (!Array.isArray(rankingTableData)) {
-//         return <div>Loading...</div>
-//     }
-
-//     const allMatchesData = allMatches.response
-
-//     const data = {
-//         matchData: allMatchesData,
-//         standingsData: rankingTableData,
-//         fetchedCurrentRound: currentRound.response
-//     }
-
-//     return (
-//         <>
-//             <Simulator
-//                 simulatorData={data}
-//             />
-//         </>
-//     )
-// }
-
 import { getLeagueStandingBySeason, getAllMatchesByLeagueSeason, getCurrentRound } from "@/lib/actions"
 import Simulator from "./Simulator"
 import LeagueCTA from "./LeagueCTA"
@@ -58,10 +11,6 @@ export default async function SimulatorFeed({ leagueId, currentSeason }: { leagu
             getCurrentRound(leagueId, currentSeason)
         ]);
 
-        // // Check for valid responses and structure
-        // if (!currentRound || !Array.isArray(currentRound.response) || currentRound.response.length < 0) {
-        //     throw new Error('Current round data is not available.');
-        // }
         if (!rankingTable?.response || !allMatches?.response) {
             throw new Error('Required league or match data is missing.');
         }
